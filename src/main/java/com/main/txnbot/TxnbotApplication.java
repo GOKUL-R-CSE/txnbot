831990eafcb1647dfd07574a12a5e617712b81b0
Gokul R <gokulrcseoficial@gmail.com>
2024-10-10 15:00:22 +0530

    initial commit

---
+++ src/main/java/com/main/txnbot/TxnbotApplication.java
@@ -0,0 +1,13 @@
+package com.main.txnbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/txnbot/TxnbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.txnbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
